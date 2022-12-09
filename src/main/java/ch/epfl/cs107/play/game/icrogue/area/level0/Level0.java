520aa9bd1237a82c361aaa4782c54b07023ff221
Mateus Vital Nabholz <mateus.vitaln@icloud.com>
2022-12-09 17:19:00 +0100

    issue with arrow

--- src/main/java/ch/epfl/cs107/play/game/icrogue/area/level0/Level0.java
+++ src/main/java/ch/epfl/cs107/play/game/icrogue/area/level0/Level0.java
@@ -10,6 +10,7 @@ import ch.epfl.cs107.play.game.icrogue.area.Level;
 import ch.epfl.cs107.play.game.icrogue.area.level0.rooms.Level0KeyRoom;
 import ch.epfl.cs107.play.game.icrogue.area.level0.rooms.Level0Room;
 import ch.epfl.cs107.play.game.icrogue.area.level0.rooms.Level0StaffRoom;
+import ch.epfl.cs107.play.game.icrogue.area.level0.rooms.Level0TurretRoom;
 import ch.epfl.cs107.play.math.DiscreteCoordinates;
 
 public class Level0 extends Level {
@@ -20,8 +21,7 @@ public class Level0 extends Level {
     }
 
     public void generateFixedMap(){
-        generateMap1();
-        generateMap2();
+        generateFinalMap();
 
     }
     private void generateMap1() {
@@ -38,6 +38,33 @@ public class Level0 extends Level {
     private void generateMap2() {
         DiscreteCoordinates room00 = new DiscreteCoordinates(0, 0);
         setRoom(room00, new Level0Room(room00));
+        setRoomConnector(room00, "icrogue/level010", Level0Room.Level0Connectors.E);
+
+        DiscreteCoordinates room10 = new DiscreteCoordinates(1,0);
+        setRoom(room10, new Level0Room(room10));
+        setRoomConnector(room10, "icrogue/level011", Level0Room.Level0Connectors.S);
+        setRoomConnector(room10, "icrogue/level020", Level0Room.Level0Connectors.E);
+
+        lockRoomConnector(room10, Level0Room.Level0Connectors.W,  2);
+        setRoomConnectorDestination(room10, "icrogue/level000", Level0Room.Level0Connectors.W);
+
+        DiscreteCoordinates room20 = new DiscreteCoordinates(2,0);
+        setRoom(room20,  new Level0StaffRoom(room20));
+        setRoomConnector(room20, "icrogue/level010", Level0Room.Level0Connectors.W);
+        setRoomConnector(room20, "icrogue/level030", Level0Room.Level0Connectors.E);
+
+        DiscreteCoordinates room30 = new DiscreteCoordinates(3,0);
+        setRoom(room30, new Level0KeyRoom(room30, 2));
+        setRoomConnector(room30, "icrogue/level020", Level0Room.Level0Connectors.W);
+
+        DiscreteCoordinates room11 = new DiscreteCoordinates(1, 1);
+        setRoom (room11, new Level0Room(room11));
+        setRoomConnector(room11, "icrogue/level010", Level0Room.Level0Connectors.N);
+    }
+
+    public void generateFinalMap(){
+        DiscreteCoordinates room00 = new DiscreteCoordinates(0, 0);
+        setRoom(room00, new Level0TurretRoom(room00));
         setRoomConnector(room00, "icrogue/level010", Level0Room.Level0Connectors.E);
 
         DiscreteCoordinates room10 = new DiscreteCoordinates(1,0);
